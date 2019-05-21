b3202ffc7a0067bf9f0eb5b647829f3c2e7f3495
Jackson Chadfield <chadfield.jackson@gmail.com>
2019-05-22 01:06:03 +1200

    Add some juicy metadata

---
+++ toolbox/__init__.py
@@ -0,0 +1,12 @@
+"""A library of utilities helping with things I've learnt in my university classes
+
+Currently only contains a vector library but hopefully more will be added throughout.
+
+Tested on python 3.7.2. May or may not work on other 3.x.x releases.
+"""
+
+__author__ = "Jackson Chadfield (chadfield.jackson@gmail.com)"
+__copyright__ = "Copyright 2019, Jackson Chadfield"
+__license__ = "MIT"
+
+__all__ = ["vectors"]
